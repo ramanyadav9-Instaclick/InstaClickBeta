222e555742669b731fc7eb52c6a693f2bd44a104
Subrat-coder98 <yadavasubrata2008@gmail.com>
2026-09-07 23:22:37 +0530

    add env to next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   images: {
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
